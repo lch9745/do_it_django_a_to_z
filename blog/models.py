c72029ad0a1de301678c2c186638618c8a9fcab3
ChanHeeLee <lch921212@gmail.com>
2021-04-22 10:37:05 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 import os
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
 
 
 class Category(models.Model):
@@ -11,9 +21,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
 
     class Meta:
-        verbose_name_plural = 'categories'  
+        verbose_name_plural = 'categories'
 
 
 class Post(models.Model):
@@ -26,19 +38,17 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #author: 추후 작성 에정
-    
-    
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+ 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
-        return f'/blog/{self.pk}/'    
-
+        return f'/blog/{self.pk}/'
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
-    #path('<int:pk>/', views.single_post_page),
     path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
 ]
-
